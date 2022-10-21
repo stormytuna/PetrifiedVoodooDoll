37eaca1adadd5e950d1164e4bfed4d5a4e43ddec
stormytuna <stormytuna@outlook.com>
2022-10-21 12:00:24 +0100

    Add new functionality to Petrified Voodoo Doll
    Lets you hit any town npc

--- Content/Items/PetrifiedVoodooDoll.cs
+++ Content/Items/PetrifiedVoodooDoll.cs
@@ -1,7 +1,6 @@
 using Terraria.GameContent.Creative;
 using Terraria.ModLoader;
 using Terraria;
-using PetrifiedVoodooDoll.Content.Projectiles;
 using Terraria.ID;
 
 namespace PetrifiedVoodooDoll.Content.Items
@@ -10,7 +9,7 @@ namespace PetrifiedVoodooDoll.Content.Items
     {
         public override void SetStaticDefaults()
         {
-            Tooltip.SetDefault("A voodoo doll protected by a stone shell to prevent any accidents...");
+            Tooltip.SetDefault("'You are the worst person'");
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
         }
 
@@ -19,14 +18,40 @@ namespace PetrifiedVoodooDoll.Content.Items
             Item.width = 16;
             Item.height = 28;
             Item.value = Item.sellPrice(silver: 2);
-            Item.useStyle = ItemUseStyleID.Swing;
-            Item.useTime = 15;
-            Item.useAnimation = 15;
-            Item.consumable = true;
-            Item.shoot = ModContent.ProjectileType<PetrifiedVoodooDollProj>();
-            Item.shootSpeed = 5f;
-            Item.maxStack = 99;
+            Item.maxStack = 1;
             Item.rare = ItemRarityID.Orange;
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<PetrifiedVoodooDollPlayer>().petrifiedVoodooDoll = true;
+        }
+    }
+
+    public class PetrifiedVoodooDollPlayer : ModPlayer
+    {
+        public bool petrifiedVoodooDoll;
+
+        public override void ResetEffects()
+        {
+            petrifiedVoodooDoll = false;
+        }
+
+        public override bool? CanHitNPC(Item item, NPC target)
+        {
+            if (petrifiedVoodooDoll && target.townNPC)
+                return true;
+
+            return base.CanHitNPC(item, target);
+        }
+
+        public override bool? CanHitNPCWithProj(Projectile proj, NPC target)
+        {
+            if (petrifiedVoodooDoll && target.townNPC)
+                return true;
+
+            return base.CanHitNPCWithProj(proj, target);
         }
     }
 }
