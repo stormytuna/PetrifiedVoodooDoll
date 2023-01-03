8a803be06b0663a80850bbc86102d13126d8eefc
stormytuna <stormytuna@outlook.com>
2023-01-03 17:14:28 +0000

    Few things

--- Content/Items/PetrifiedVoodooDoll.cs
+++ Content/Items/PetrifiedVoodooDoll.cs
@@ -1,53 +1,53 @@
-using Terraria.GameContent.Creative;
-using Terraria.ModLoader;
 using Terraria;
+using Terraria.GameContent.Creative;
 using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Utilities;
 
-namespace PetrifiedVoodooDoll.Content.Items
-{
-    public class PetrifiedVoodooDoll : ModItem
-    {
-        public override void SetStaticDefaults()
-        {
+namespace PetrifiedVoodooDoll.Content.Items {
+    public class PetrifiedVoodooDoll : ModItem {
+        public override void SetStaticDefaults() {
             Tooltip.SetDefault("'You are the worst person'");
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
         }
 
-        public override void SetDefaults()
-        {
+        public override void SetDefaults() {
             Item.width = 16;
             Item.height = 28;
             Item.value = Item.sellPrice(silver: 2);
-            Item.maxStack = 1;
+            Item.maxStack = 99;
             Item.rare = ItemRarityID.Orange;
             Item.accessory = true;
         }
 
-        public override void UpdateAccessory(Player player, bool hideVisual)
-        {
+        public override bool? PrefixChance(int pre, UnifiedRandom rand) {
+            if (pre == -1) {
+                return false;
+            }
+
+            return base.PrefixChance(pre, rand);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual) {
             player.GetModPlayer<PetrifiedVoodooDollPlayer>().petrifiedVoodooDoll = true;
         }
     }
 
-    public class PetrifiedVoodooDollPlayer : ModPlayer
-    {
+    public class PetrifiedVoodooDollPlayer : ModPlayer {
         public bool petrifiedVoodooDoll;
 
-        public override void ResetEffects()
-        {
+        public override void ResetEffects() {
             petrifiedVoodooDoll = false;
         }
 
-        public override bool? CanHitNPC(Item item, NPC target)
-        {
+        public override bool? CanHitNPC(Item item, NPC target) {
             if (petrifiedVoodooDoll && target.townNPC)
                 return true;
 
             return base.CanHitNPC(item, target);
         }
 
-        public override bool? CanHitNPCWithProj(Projectile proj, NPC target)
-        {
+        public override bool? CanHitNPCWithProj(Projectile proj, NPC target) {
             if (petrifiedVoodooDoll && target.townNPC)
                 return true;
 
